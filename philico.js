6538c75c51f986a39bd3a287fc0df14f19a8dbad
walidbenhammoud <walidosbh@gmail.com>
2015-12-19 16:51:42 +0100

    Version: meeting with Fabian 18.12.2015

--- philico.js
+++ philico.js
@@ -6,11 +6,26 @@ Degrees = new Mongo.Collection("degrees");
 Languages = new Mongo.Collection("languages");
 
 
-/*var imageStore = new FS.Store.GridFS("images");
+var imageStore = new FS.Store.GridFS("images");
 
 Images = new FS.Collection("images", {
  stores: [imageStore]
 });
+
+Images.deny({
+ insert: function(){
+ return false;
+ },
+ update: function(){
+ return false;
+ },
+ remove: function(){
+ return false;
+ },
+ download: function(){
+ return false;
+ }
+ });
 
 Images.allow({
  insert: function(){
@@ -25,26 +40,28 @@ Images.allow({
  download: function(){
  return true;
  }
-});*/
+});
 
 if (Meteor.isClient) {
+
   // This code only runs on the client
   Template.body.helpers({
     
     initialisation: function () {
-      if (Personalinfo.find({ _id: Meteor.user().emails[0].address}).count() === 0) {
+      /*Meteor.call("removeAllPosts");*/
+      if (Personalinfo.find({ _id: Meteor.user().services.google.email}).count() === 0) {
       Personalinfo.insert({
-        _id: Meteor.user().emails[0].address,
-        email: Meteor.user().emails[0].address,
-        finame: "",
-        faname: "",
+        _id: Meteor.user().services.google.email,
+        email: Meteor.user().services.google.email,
+        finame: Meteor.user().services.google.given_name,
+        faname: Meteor.user().services.google.family_name,
         street: "",
         zip: "",
         city: "",
         nationality1: "",
         nationality2: "",
         nationality3: "",
-        sourcetaxed: "no",
+        sourcetaxed: "?",
         dnumber: "",
         mnumber: "",
         birth: "",
@@ -78,6 +95,7 @@ if (Meteor.isClient) {
         euriban: "",
         euraccnbr: "",
         euraccname: "",
+        createdAt: new Date(),
         competencies: new Array(),  
         experiences: new Array(),
         employments: new Array(),
@@ -87,68 +105,72 @@ if (Meteor.isClient) {
     }
     },
 
+    adminrights: function () {
+      if(Meteor.user().services.google.email==="walid.benhammoud@philico.com") {
+        return true
+      } else {
+        return false
+      }
+    },
+
+    adminpersonalinfo: function () {
+      return Personalinfo.find({})},
+
     personalinfo: function () {
-      return Personalinfo.find({_id: Meteor.user().emails[0].address})},
+      return Personalinfo.find({_id: Meteor.user().services.google.email})},
 
     competencies: function () {
-      return Competencies.find({owner: Meteor.user().emails[0].address})},
+      return Competencies.find({owner: Meteor.user().services.google.email})},
 
     experiences: function () {
-      return Experiences.find({owner: Meteor.user().emails[0].address})},
+      return Experiences.find({owner: Meteor.user().services.google.email})},
 
     employments: function () {
-      return Employments.find({owner: Meteor.user().emails[0].address})},
+      return Employments.find({owner: Meteor.user().services.google.email})},
 
     degrees: function () {
-      return Degrees.find({owner: Meteor.user().emails[0].address})},
+      return Degrees.find({owner: Meteor.user().services.google.email})},
 
     languages: function () {
-      return Languages.find({owner: Meteor.user().emails[0].address})},
+      return Languages.find({owner: Meteor.user().services.google.email})},
    
   });
 
 
   
 Template.body.events({
- /* "submit .new-cv": function (event) {
-    function genPDF() {
-  html2canvas(document.body, {
-    onrendered: function (canvas) {
-
-      var image = canvas.toDataURL("image/png");
-      var doc = new jsPDF();
-      doc.addImage(image, 'JPEG',20,20);
-      doc.save('test.pdf');
-      }
-    });
-    }
-  },*/
 
     "submit .newcv": function(event) {
       event.preventDefault();
-      Blaze.saveAsPDF(Template.cvtemp, {
+      var doc = new jsPDF();
+      doc.fromHTML($('#mycv').get(0), 20,20,{ 'width': 500});
+      doc.save('CV.pdf');
+      /*Blaze.saveAsPDF(Template.cvtemp, {
       filename: "cv.pdf", // optional, default is "document.pdf"
-       // optional, render the template with this data context
-      x: 0, // optional, left starting position on resulting PDF, default is 4 units
-      y: 0, // optional, top starting position on resulting PDF, default is 4 units
+      x: 1, // optional, left starting position on resulting PDF, default is 4 units
+      y: 1, // optional, top starting position on resulting PDF, default is 4 units
       orientation: "portrait", // optional, "landscape" or "portrait" (default)
-      unit: "in", // optional, unit for coordinates, one of "pt", "mm" (default), "cm", or "in"
-      format: "letter" // optional, see Page Formats, default is "a4"
-    });
+      unit: "cm", // optional, unit for coordinates, one of "pt", "mm" (default), "cm", or "in"
+      format: "a4" // optional, see Page Formats, default is "a4"
+    });*/
     },
 
     "change .myFileInput": function(event, template) {
       FS.Utility.eachFile(event, function(file) {
         Images.insert(file, function (err, fileObj) {
           if (err){
-             // handle error
+             console.log('error image wal')
           } else {
              // handle success depending what you need to do
+            console.log('error image 1');
             var userId = Meteor.userId();
+            console.log('error image 2');
             var imagesURL = {
-              "profile.image": "/Philico/philico/images/" + fileObj._id
+              "profile.image": "/public/img" + fileObj._id
             };
+            console.log('error image 3');
             Meteor.users.update(userId, {$set: imagesURL});
+            console.log('error image 4');
           }
         })
       })
@@ -157,8 +179,6 @@ Template.body.events({
       "submit .new-profile": function (event) {
         // Prevent default browser form submit
       event.preventDefault();
-      var finame=event.target.finame.value;
-      var faname=event.target.faname.value;
       var street=event.target.street.value;
       var zip=event.target.zip.value;
       var city=event.target.city.value;
@@ -168,9 +188,11 @@ Template.body.events({
       var sourcetaxed=event.target.sourcetaxed.value;
       var dnumber=event.target.dnumber.value;
       var mnumber=event.target.mnumber.value;
-      var birth=event.target.birth.value;
+      var myDocument = Personalinfo.findOne({ _id: Meteor.user().services.google.email });
+      if (myDocument) {var birth=myDocument.birth} else {birth=event.target.birth.value};
       var ahv=event.target.ahv.value;
-      var sdate=event.target.sdate.value;
+      var myDocument = Personalinfo.findOne({ _id: Meteor.user().services.google.email });
+      if (myDocument) {var sdate=myDocument.sdate} else {sdate=event.target.sdate.value};
       var phposition=event.target.phposition.value;
       var mdate=event.target.mdate.value;
       var sfaname=event.target.sfaname.value;
@@ -200,14 +222,13 @@ Template.body.events({
       var euraccnbr=event.target.euraccnbr.value;
       var euraccname=event.target.euraccname.value;
       
-      Meteor.call("addPersonalinfo", finame, faname, street, zip, city, nationality1, nationality2, nationality3,
+      Meteor.call("addPersonalinfo", street, zip, city, nationality1, nationality2, nationality3,
        sourcetaxed, dnumber, mnumber, birth, ahv, sdate, phposition, mdate, sfaname, sfiname, ch1name, ch1bdate, 
        ch2name, ch2bdate, ch3name, ch3bdate, em1name, relation1, phone1, em2name, relation2, phone2, bankname, 
        bankplace, bankzip, iban, accnbr, accname, eurbankname, eurbankplace, eurbankzip, euriban, euraccnbr, euraccname);
       
-      event.target.finame.value="";
-      event.target.faname.value="";
-      event.target.street.value="";
+
+      /*event.target.street.value="";
       event.target.zip.value="";
       event.target.city.value="";
       event.target.nationality1.value="";
@@ -245,7 +266,7 @@ Template.body.events({
       event.target.eurbankzip.value="";
       event.target.euriban.value="";
       event.target.euraccnbr.value="";
-      event.target.euraccname.value="";
+      event.target.euraccname.value="";*/
         },
 
       "submit .new-competency": function (event) {
@@ -256,9 +277,9 @@ Template.body.events({
 
       Meteor.call("addCompetency", comskill, businessskill, technicalskill);
       // Clear form
-      event.target.comskill.value = "";
+      /*event.target.comskill.value = "";
       event.target.businessskill.value = "";
-      event.target.technicalskill.value = "";
+      event.target.technicalskill.value = "";*/
     },
 
      "submit .new-experience": function (event) {
@@ -316,7 +337,6 @@ Template.body.events({
       event.target.empcompany.value="";
       event.target.empcity.value="";
       event.target.empposition.value="";
-
     },
 
       "submit .new-degree": function (event) {
@@ -358,54 +378,53 @@ Template.body.events({
       Meteor.call("deleteLanguage", this._id);
     },
   });
-  
 
-Accounts.ui.config({
-  passwordSignupFields: "EMAIL_ONLY"
-});
- 
-/*Template.register.events({
-        'submit form': function(event) {
-            event.preventDefault();
-            var message = document.getElementById("regmail");
-            message.innerHTML= "";
-            var emailVar = event.target.registerEmail.value;
-            var passwordVar = event.target.registerPassword.value;
-            if (emailVar.indexOf('@philico.com') == -1) {message.innerHTML= "Please use your Philico's email address"}
-           else{
-            Accounts.createUser({
-            _id: emailVar,
-            email: emailVar,
-            password: passwordVar
-        });
-        }
-        }
-    });*/
+    Accounts.ui.config({
+    passwordSignupFields: "EMAIL_ONLY"
+  });
+
 
 };
+
 
 if (Meteor.isServer) {
   Meteor.startup(function() {
     return Meteor.methods({
       removeAllPosts: function() {
-        return Experiences.remove({});
+        /*Meteor.users.remove({});*/
+        Personalinfo.remove({});
       }
     });
   });
-  };
+
+
+  Accounts.config({
+    restrictCreationByEmailDomain: 'philico.com'
+  });
+
+  ServiceConfiguration.configurations.remove({
+    service: "google"
+  });
+  
+  ServiceConfiguration.configurations.insert({
+    service: "google",
+    requestPermissions: ['email', 'given_name', 'family_name'],
+    clientId: "315507517489-36bp2qcmrfjuq43tj23jvuqq1qf6uc7f.apps.googleusercontent.com",
+    secret: "E61FAWeNc8YSjSPt_XFtIPP5"
+  });
+
+};
   
 
 Meteor.methods({
 
-  addPersonalinfo: function(finame, faname, street, zip, city, nationality1, nationality2, nationality3,
+  addPersonalinfo: function(street, zip, city, nationality1, nationality2, nationality3,
      sourcetaxed, dnumber, mnumber, birth, ahv, sdate, phposition, mdate, sfaname, sfiname, ch1name, ch1bdate, ch2name, ch2bdate,
      ch3name, ch3bdate, em1name, relation1, phone1, em2name, relation2, phone2, bankname, bankplace, bankzip, iban,
      accnbr, accname, eurbankname, eurbankplace, eurbankzip, euriban, euraccnbr, euraccname) {
-    Personalinfo.update(Meteor.user().emails[0].address,
+    Personalinfo.update(Meteor.user().services.google.email,
       { $set: 
         {
-     finame: finame,
-     faname: faname,
      street: street,
      zip: zip,
      city: city,
@@ -452,28 +471,28 @@ Meteor.methods({
   },
 
   addCompetency: function(comskill, businessskill, technicalskill) {
-    var myDocument = Competencies.findOne({ owner: Meteor.user().emails[0].address });
+    var myDocument = Competencies.findOne({ owner: Meteor.user().services.google.email });
     if (myDocument) {
       var deletedcomskill = myDocument.comskill;
       var deletedbusinessskill = myDocument.businessskill;
       var deletedtechnicalskill = myDocument.technicalskill;
-      Competencies.remove({owner: Meteor.user().emails[0].address});
+      Competencies.remove({owner: Meteor.user().services.google.email});
       Personalinfo.update(
-    {_id: Meteor.user().emails[0].address },
+    {_id: Meteor.user().services.google.email },
     { $pull: { competencies: [deletedcomskill, deletedbusinessskill, deletedtechnicalskill ] } },
     { multi: true }
     )
     };
 
     Competencies.insert({
-      owner: Meteor.user().emails[0].address,
+      owner: Meteor.user().services.google.email,
       comskill: comskill,
       businessskill: businessskill,
       technicalskill: technicalskill,
       createdAt: new Date(),
     });
 
-    Personalinfo.update(Meteor.user().emails[0].address,
+    Personalinfo.update(Meteor.user().services.google.email,
     { $addToSet: {competencies: [comskill, businessskill, technicalskill ] } }
     )
   },
@@ -481,7 +500,7 @@ Meteor.methods({
   addExperience: function(company, stdate, enddate, customer, customercity, department, projecttitle1, projecttitle2, 
         projecttitle3, projecttitle4, projectdescription1, projectdescription2, projectdescription3, projectdescription4) {
     Experiences.insert({
-        owner: Meteor.user().emails[0].address,
+        owner: Meteor.user().services.google.email,
         company: company, 
         stdate: stdate, 
         enddate: enddate, 
@@ -499,7 +518,7 @@ Meteor.methods({
         createdAt: new Date(),  
       });
 
-    Personalinfo.update(Meteor.user().emails[0].address,
+    Personalinfo.update(Meteor.user().services.google.email,
     { $addToSet: {experiences: [company, stdate, enddate, customer, customercity, department, projecttitle1, projecttitle2, 
         projecttitle3, projecttitle4, projectdescription1, projectdescription2, projectdescription3, projectdescription4 ] } }
     )   
@@ -524,7 +543,7 @@ Meteor.methods({
         var deletedprojectdescription4= myDocument.projectdescription4;
     }
     Personalinfo.update(
-    { _id: Meteor.user().emails[0].address },
+    { _id: Meteor.user().services.google.email },
     { $pull: { experiences: [deletedcompany, deletedstdate, deletedenddate, deletedcustomer, deletedcustomercity, 
       deleteddepartment, deletedprojecttitle1, deletedprojecttitle2, deletedprojecttitle3, deletedprojecttitle4, 
       deletedprojectdescription1, deletedprojectdescription2, deletedprojectdescription3, deletedprojectdescription4 ] } },
@@ -533,9 +552,9 @@ Meteor.methods({
     Experiences.remove(id);
   },
 
-  addEmployment: function(empstdate, empenddate, empcompany, empposition) {
+  addEmployment: function(empstdate, empenddate, empcompany, empcity, empposition) {
     Employments.insert({
-          owner: Meteor.user().emails[0].address,
+          owner: Meteor.user().services.google.email,
           empstdate: empstdate,
           empenddate: empenddate,
           empcompany: empcompany,
@@ -544,7 +563,7 @@ Meteor.methods({
           createdAt: new Date(),            // current time
       });
 
-    Personalinfo.update(Meteor.user().emails[0].address,
+    Personalinfo.update(Meteor.user().services.google.email,
     { $addToSet: {employments: [empstdate, empenddate, empcompany, empcity, empposition ] } }
     )   
   },
@@ -559,7 +578,7 @@ Meteor.methods({
       var deletedempposition = myDocument.empposition;
     }
     Personalinfo.update(
-    { _id: Meteor.user().emails[0].address },
+    { _id: Meteor.user().services.google.email },
     { $pull: { employments: [deletedempstdate, deletedempenddate, deletedempcompany, deletedempcity, deletedempposition ] } },
     { multi: true }
     );
@@ -568,14 +587,14 @@ Meteor.methods({
   
   addDegree: function(ddate, dip, uni) {
     Degrees.insert({
-          owner: Meteor.user().emails[0].address,
+          owner: Meteor.user().services.google.email,
           ddate: ddate,
           dip: dip,
           uni: uni,
           createdAt: new Date(),            // current time
       });
 
-    Personalinfo.update(Meteor.user().emails[0].address,
+    Personalinfo.update(Meteor.user().services.google.email,
     { $addToSet: {degrees: [ddate, dip, uni ] } }
     )   
   },
@@ -588,7 +607,7 @@ Meteor.methods({
       var deleteduni = myDocument.uni;
     }
     Personalinfo.update(
-    { _id: Meteor.user().emails[0].address },
+    { _id: Meteor.user().services.google.email },
     { $pull: { degrees: [deletedddate, deleteddip, deleteduni ] } },
     { multi: true }
     );
@@ -597,12 +616,12 @@ Meteor.methods({
 
   addLanguage: function(langue, skill) {
     Languages.insert({
-          owner: Meteor.user().emails[0].address,
+          owner: Meteor.user().services.google.email,
           langue: langue,
           skill: skill,
           createdAt: new Date(),            // current time
       });
-    Personalinfo.update(Meteor.user().emails[0].address,
+    Personalinfo.update(Meteor.user().services.google.email,
     { $addToSet: {languages: [ langue, skill ] } } 
     )
   },
@@ -614,7 +633,7 @@ Meteor.methods({
       var deletedskill = myDocument.skill;
 
     Personalinfo.update( 
-    { _id: Meteor.user().emails[0].address },
+    { _id: Meteor.user().services.google.email },
     { $pull: { languages: [ deletedlanguage, deletedskill ] } },
     { multi: true }
     );
